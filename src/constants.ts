feb8acefc75585c087b8af13fd54f50dcb866076
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-08-07 18:49:46 -0400

    🗃️ add fields to field map

--- src/constants.ts
+++ src/constants.ts
@@ -12,5 +12,23 @@ export const FIELD_TYPE_MAP: Record<string, string> = {
   checkbox: "z.boolean().optional()",
   date: "z.string().optional()",
   dateTime: "z.string().optional()",
+  createdTime: "z.string().optional()",
+  lastModifiedTime: "z.string().optional()",
+  percent: "z.number().optional()",
+  rating: "z.number().min(1).optional()",
+  currency: "z.number().positive().optional()",
+  button: "z.object({ label: z.string(), url: z.string() }).optional()",
+  createdBy:
+    "z.object({ id: z.string(), name: z.string(), email: z.string() }).optional()",
+  formula:
+    "z.union([z.string(), z.number(), z.array(z.union([z.string(), z.number()]))]).optional()",
+  rollup: "z.union([z.string(), z.number()]).optional()",
+  lookup: "z.string().optional()",
+  singleSelect: "z.string().optional()",
+  multipleSelects: "z.string().array().optional()",
+  multipleRecordLinks: "z.string().array().optional()",
   default: "z.any().optional()",
 }
+
+// multipleLookupValues
+// multipleAttachments
